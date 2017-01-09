fa2f69605b22c918b157e06e91b33abde26bdc40
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-01-09 20:02:27 +0100

    Completed json api call

--- wiki.js
+++ wiki.js
@@ -1,14 +1,24 @@
 function search() {
-  var title = $("#search-text").val();
+    var title = $("#search-text").val();
 
-  var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-  var cb = '&callback=JSON_CALLBACK';
-  var page = 'https://en.wikipedia.org/?curid=';
+    var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var cb = '&callback=JSON_CALLBACK';
+    var page = 'https://en.wikipedia.org/?curid=';
 
-  $.getJSON(api + title + cb)
-    .success(function(data) {
-      var results = data.query.pages;
-      console.log(results);
+
+    $.ajax({
+        url: api + title + cb,
+
+        // The name of the callback parameter, as specified by the YQL service
+        jsonp: "callback",
+
+        // Tell jQuery we're expecting JSONP
+        dataType: "jsonp",
+
+        // Work with the response
+        success: function(data) {
+            var results
+            console.log(data.query.pages); // server response
+        }
     });
-
 }
