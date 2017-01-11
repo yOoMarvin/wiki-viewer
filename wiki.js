d8673e1a5e0f3d6dc856e5cc21cfd948b52d8745
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-01-11 20:40:53 +0100

    Added delete function and comments

--- wiki.js
+++ wiki.js
@@ -1,25 +1,28 @@
 function search() {
+    //delete every div with class result, so there are now doubled entries
+    $('.result').remove();
+
+    //search value (title)
     var title = $("#search-text").val();
 
+    //url and api request variables
     var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
     var cb = '&callback=JSON_CALLBACK';
     var page = 'https://en.wikipedia.org/?curid=';
 
 
     $.ajax({
+        //buil url
         url: api + title + cb,
-
         jsonp: "callback",
-
         // Tell jQuery we're expecting JSONP
         dataType: "jsonp",
-
         // Work with the response
         success: function(data) {
             //server response
             var results = data.query.pages;
             console.log(results);
-
+            //for each result add a new node
             $.each( results, function( key, value ) {
               $('.container').append('<a href='+page+value.pageid+
               '<div class=result id='+ key +'>'+
